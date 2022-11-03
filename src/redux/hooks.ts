64902c0979b6e91077af58fd49b3c5295fce0f76
Sejfudin <sejfudindu@maestralsolutions.com>
2022-11-03 13:57:25 +0100

    Code refactoring, and router added

--- src/redux/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { useSelector } from "react-redux"
-import { TypedUseSelectorHook, useDispatch } from "react-redux"
-import { AppDispatch, RootStore } from "./store"
-
-export const useAppDispatch: () => AppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootStore> = useSelector
--- src/redux/reducers/RootReducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import  pageReducer  from "./PageReducer";
-
-const RootReducer = combineReducers({
-   pages: pageReducer
-})
-
-export default RootReducer;
